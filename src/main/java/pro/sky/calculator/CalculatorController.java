964505d8fba746276ea702411c800ded19cf5869
Zelioforest <veshky@mail.ru>
2024-01-22 03:06:15 +0300

    only calc

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,10 +1,12 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
@@ -13,13 +15,26 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
-    @GetMapping(path = "/calculator")
+    @GetMapping()
     public String GreatingCalculator(){
         return calculatorService.GreatingCalculator();
     }
 
-    @GetMapping(path = "/calculator/plus")
-    public String Calculatorsum(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+    @GetMapping("/plus")
+    public String CalculatorPlus(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
         return CalculatorService.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String CalculatorMinus(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return CalculatorService.minus(num1, num2);
+    }
+    @GetMapping("/multiply")
+    public String CalculatorMultiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return CalculatorService.multiply(num1, num2);
+    }
+    @GetMapping("/divide")
+    public String CalculatorDivide(@RequestParam("num1") int num1, @RequestParam("num2") int num2){
+        return CalculatorService.divide(num1, num2);
     }
 }
--- src/main/java/pro/sky/calculator/CalculatorService.java
+++ src/main/java/pro/sky/calculator/CalculatorService.java
@@ -4,12 +4,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+
     public String GreatingCalculator(){
         return "Welcome to Calculator";
     }
 
+    public static String ourStr(int n1, int n2, String znak, int res){
+        return Integer.toString(n1) + znak + Integer.toString(n2) + " = " + Integer.toString(res);
+    }
+
     public static String plus(int num1, int num2){
         int res = num1 + num2;
-        return Integer.toString(num1) + " + " + Integer.toString(num2) + " = " + Integer.toString(res);
+        return ourStr(num1, num2, " + ", res);
+    }
+
+    public static String minus(int num1, int num2){
+        int res = num1 - num2;
+        return ourStr(num1, num2, " - ", res);
+    }
+
+    public static String multiply(int num1, int num2){
+        int res = num1 * num2;
+        return ourStr(num1, num2, " * ", res);
+    }
+
+    public static String divide(int num1, int num2){
+        int res = num1 / num2;
+        return ourStr(num1, num2, " / ", res);
     }
 }
